c3814d66d56bda3072cf2ae6b3669d0ad41e8e85
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-06-04 14:24:20 +0530

    Create CountVowelsConsonants.java

---
+++ CountVowelsConsonants.java
@@ -0,0 +1,22 @@
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        String s = "programming";
+        int vowels = 0, consonants = 0;
+
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if ("aeiou".indexOf(ch) != -1)
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
