1bb87ee134a33906cd652f87aa9d779a755e92e9
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-06-14 14:39:40 +0530

    Create EvenOddBitCheck.java

---
+++ EvenOddBitCheck.java
@@ -0,0 +1,11 @@
+public class EvenOddBitCheck {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if ((num & 1) == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+        }
+    }
+}
