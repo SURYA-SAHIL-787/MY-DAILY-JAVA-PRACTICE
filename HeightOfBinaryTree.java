925940268e3a903dd4fc1ec86b2c09de50ce6d5d
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-06-16 19:35:27 +0530

    Create HeightOfBinaryTree.java

---
+++ HeightOfBinaryTree.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class HeightOfBinaryTree {
+    static int[] value;
+    static int[] left;
+    static int[] right;
+
+    public static int height(int node) {
+        if (node == -1) {
+            return 0;
+        }
+
+        int leftHeight = height(left[node]);
+        int rightHeight = height(right[node]);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        value = new int[n];
+        left = new int[n];
+        right = new int[n];
+
+        System.out.println("Enter value, left child, right child for each node:");
+        System.out.println("Use -1 if child does not exist");
+
+        for (int i = 0; i < n; i++) {
+            value[i] = sc.nextInt();
+            left[i] = sc.nextInt();
+            right[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter root node index: ");
+        int root = sc.nextInt();
+
+        System.out.println("Height of tree: " + height(root));
+
+        sc.close();
+    }
+}
