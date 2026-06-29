2b4d50d92a156a5eda1a68472db92fa56b9252f7
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-06-29 20:24:52 +0530

    Create MaxNonAdjacentSum.java

---
+++ MaxNonAdjacentSum.java
@@ -0,0 +1,29 @@
+public class MaxNonAdjacentSum {
+    public static int maxSum(int[] arr) {
+        int n = arr.length;
+
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+        int prev2 = 0;
+        int prev1 = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            int pick = arr[i] + prev2;
+            int notPick = prev1;
+
+            int curr = Math.max(pick, notPick);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 4, 9};
+
+        System.out.println("Maximum Non-Adjacent Sum: " + maxSum(arr));
+    }
+}
