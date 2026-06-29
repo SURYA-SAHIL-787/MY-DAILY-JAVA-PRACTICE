1d214dcbedc768acbdd29d8f6494e13a02e7fa98
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-06-29 20:25:14 +0530

    Create CountWaysToReachEnd.java

---
+++ CountWaysToReachEnd.java
@@ -0,0 +1,23 @@
+public class CountWaysToReachEnd {
+    public static int countWays(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        int prev2 = 1;
+        int prev1 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Number of ways: " + countWays(n));
+    }
+}
