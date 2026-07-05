702ea46a4addd7025ddc810dd8b583519448441a
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-07-05 10:01:12 +0530

    Create TernarySearch.java

---
+++ TernarySearch.java
@@ -0,0 +1,37 @@
+public class TernarySearch {
+    public static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid1 = left + (right - left) / 3;
+            int mid2 = right - (right - left) / 3;
+
+            if (arr[mid1] == target) {
+                return mid1;
+            }
+
+            if (arr[mid2] == target) {
+                return mid2;
+            }
+
+            if (target < arr[mid1]) {
+                right = mid1 - 1;
+            } else if (target > arr[mid2]) {
+                left = mid2 + 1;
+            } else {
+                left = mid1 + 1;
+                right = mid2 - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11, 13};
+        int target = 9;
+
+        System.out.println(search(arr, target));
+    }
+}
