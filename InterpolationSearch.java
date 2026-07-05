376811db14d25a5e4c5fb408275175e45549a1b2
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-07-05 09:59:54 +0530

    Create InterpolationSearch.java

---
+++ InterpolationSearch.java
@@ -0,0 +1,36 @@
+public class InterpolationSearch {
+    public static int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high && target >= arr[low] && target <= arr[high]) {
+            if (arr[low] == arr[high]) {
+                if (arr[low] == target) {
+                    return low;
+                }
+                return -1;
+            }
+
+            int pos = low + ((target - arr[low]) * (high - low)) / (arr[high] - arr[low]);
+
+            if (arr[pos] == target) {
+                return pos;
+            }
+
+            if (arr[pos] < target) {
+                low = pos + 1;
+            } else {
+                high = pos - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 40;
+
+        System.out.println(search(arr, target));
+    }
+}
