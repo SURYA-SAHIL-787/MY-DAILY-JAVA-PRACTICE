3a0e0e059c8b01e0654af7384883bcb70514f3a7
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-08-12 21:47:24 +0530

    Create PerfectTreeTraversal.java

---
+++ PerfectTreeTraversal.java
@@ -0,0 +1,62 @@
+public class PerfectTreeTraversal {
+
+    static class Node {
+
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void preorder(Node root) {
+
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    static void inorder(Node root) {
+
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    static void postorder(Node root) {
+
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.print("Preorder: ");
+        preorder(root);
+
+        System.out.print("\nInorder: ");
+        inorder(root);
+
+        System.out.print("\nPostorder: ");
+        postorder(root);
+    }
+}
