032f90b76176856c2344c04c01f8e084ab0af4da
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-09-26 20:57:29 +0530

    Create RightTriangle.java

---
+++ RightTriangle.java
@@ -0,0 +1,14 @@
+public class RightTriangle {
+    public static void printRightTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printRightTriangle(5);
+    }
+}
