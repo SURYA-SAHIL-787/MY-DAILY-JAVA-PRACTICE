48d77189aee0bc07066870ffd0e720d37e86afeb
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-09-26 20:58:58 +0530

    Add HollowSquare class to print hollow squares

---
+++ HollowSquare.java
@@ -0,0 +1,18 @@
+public class HollowSquare {
+    public static void printHollowSquare(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printHollowSquare(5);
+    }
+}
