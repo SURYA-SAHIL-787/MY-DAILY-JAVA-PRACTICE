eeac38addb45f88be172e3644f7d9622a516bf39
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-09-26 20:57:52 +0530

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,19 @@
+public class Pyramid {
+    public static void printPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printPyramid(5);
+    }
+}
